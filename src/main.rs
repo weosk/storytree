8e5012b5e2b41fb8af07e16de15aaacb8563e5d4
weosk <satzger.benjamin@gmail.com>
2023-09-05 12:39:29 +0200

    branchtest treebuilder

--- src/main.rs
+++ src/main.rs
@@ -198,88 +198,88 @@ fn setup(
 
     let mut add_indi: u32 = 0;
 
-    for entry in WalkDir::new("/home/ben/projects/").into_iter().filter_map(|e| e.ok()) {
-        countVertices += 12;
-        if entry.file_type().is_dir() 
-        {
-            // Dive Up and to the side, depending on Directory
-            for each in ground_vertices {
+    // for entry in WalkDir::new("/home/ben/projects/").into_iter().filter_map(|e| e.ok()) {
+    //     countVertices += 12;
+    //     if entry.file_type().is_dir() 
+    //     {
+    //         // Dive Up and to the side, depending on Directory
+    //         for each in ground_vertices {
                 
-                vertexvec.push(   ( // Rotation * Translation -> Transform TriangleVertex -> into Vec<[f32; 3]>
-                                    Affine3A::from_quat(Quat::from_rotation_y(rot)) *
-                                    Affine3A::from_translation(Vec3{x:cntVert*0.02,y:cnt*0.02,z:0.0})
-                                  )
-                                  .transform_point3( Vec3::from_array(each) ) 
-                                  .into()
-            );
-            }
+    //             vertexvec.push(   ( // Rotation * Translation -> Transform TriangleVertex -> into Vec<[f32; 3]>
+    //                                 Affine3A::from_quat(Quat::from_rotation_y(rot)) *
+    //                                 Affine3A::from_translation(Vec3{x:cntVert*0.02,y:cnt*0.02,z:0.0})
+    //                               )
+    //                               .transform_point3( Vec3::from_array(each) ) 
+    //                               .into()
+    //         );
+    //         }
 
-            // dublicate indizes
-            add_indi = 12 * (cnt as u32);
-            indexvec.extend(vec![  0+add_indi, 2+add_indi, 1+add_indi, 
-                                         3+add_indi, 4+add_indi, 5+add_indi, 
-                                         6+add_indi, 7+add_indi, 8+add_indi, 
-                                         9+add_indi, 11+add_indi, 10+add_indi]); 
+    //         // dublicate indizes
+    //         add_indi = 12 * (cnt as u32);
+    //         indexvec.extend(vec![  0+add_indi, 2+add_indi, 1+add_indi, 
+    //                                      3+add_indi, 4+add_indi, 5+add_indi, 
+    //                                      6+add_indi, 7+add_indi, 8+add_indi, 
+    //                                      9+add_indi, 11+add_indi, 10+add_indi]); 
 
-            // println!("Dir: {:?}", entry.path());
-            cnt += 1.0;
-        }
-        else if entry.file_type().is_file() 
-        {
-            cntVert += 1.0; 
-            //println!("Filename: {:?} \n {:?} \n", entry.file_name(), entry.metadata());
+    //         // println!("Dir: {:?}", entry.path());
+    //         cnt += 1.0;
+    //     }
+    //     else if entry.file_type().is_file() 
+    //     {
+    //         cntVert += 1.0; 
+    //         //println!("Filename: {:?} \n {:?} \n", entry.file_name(), entry.metadata());
 
-            if cnt != cntOld 
-            {
-                rot=rot+1.0;
-                cntVert *= -1.0;
-                cntOld = cnt; 
-            }
-        }
-        else if entry.file_type().is_symlink()
-        {
-            // println!("Symlink: {:?}", entry.file_name());
-        }
-        else 
-        {
-            //println!("Not Dir nor File: {:?}", entry.file_name());
-        }
-    }
+    //         if cnt != cntOld 
+    //         {
+    //             rot=rot+1.0;
+    //             cntVert *= -1.0;
+    //             cntOld = cnt; 
+    //         }
+    //     }
+    //     else if entry.file_type().is_symlink()
+    //     {
+    //         // println!("Symlink: {:?}", entry.file_name());
+    //     }
+    //     else 
+    //     {
+    //         //println!("Not Dir nor File: {:?}", entry.file_name());
+    //     }
+    // }
 
-    println!("Verti: {}", countVertices);
+    // println!("Verti: {}", countVertices);
 
-    // let mut mesh_vec = vec![];
-    // mesh_vec.extend(vec![[-1., 0., 0.], [0., 1., 0.], [1., 0., 0.] ]);
-    // mesh_vec.extend(vec![[-1., 0., 0.], [0., -1., 0.], [1., 0., 0.]]);
-    // mesh_vec.extend(vec![[-1., 0., 0.], [0., 1., 0.], [1., 0., 0.] ]);
-    // mesh_vec.extend(vec![[-1., 0., 0.], [0., -1., 0.], [1., 0., 0.]]);
+    // // let mut mesh_vec = vec![];
+    // // mesh_vec.extend(vec![[-1., 0., 0.], [0., 1., 0.], [1., 0., 0.] ]);
+    // // mesh_vec.extend(vec![[-1., 0., 0.], [0., -1., 0.], [1., 0., 0.]]);
+    // // mesh_vec.extend(vec![[-1., 0., 0.], [0., 1., 0.], [1., 0., 0.] ]);
+    // // mesh_vec.extend(vec![[-1., 0., 0.], [0., -1., 0.], [1., 0., 0.]]);
 
-    mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, vec![[0., 1., 0.]; vertexvec.len()]);
-    mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, vec![[0., 0.]; vertexvec.len()]);
+    // mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, vec![[0., 1., 0.]; vertexvec.len()]);
+    // mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, vec![[0., 0.]; vertexvec.len()]);
     
-    println!("vertexvecLen: {}", vertexvec.len());
-    println!("indexvecLen: {}", indexvec.len());
+    // println!("vertexvecLen: {}", vertexvec.len());
+    // println!("indexvecLen: {}", indexvec.len());
 
-    mesh.insert_attribute(
-        Mesh::ATTRIBUTE_POSITION,
-        vertexvec,
-    );
+    // mesh.insert_attribute(
+    //     Mesh::ATTRIBUTE_POSITION,
+    //     vertexvec,
+    // );
 
-    // In this example, normals and UVs don't matter,
-    // so we just use the same value for all of them
-    // mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, vec![[0., 1., 0.]; 12]);
-    // mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, vec![[0., 0.]; 12]);
+    // // In this example, normals and UVs don't matter,
+    // // so we just use the same value for all of them
+    // // mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, vec![[0., 1., 0.]; 12]);
+    // // mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, vec![[0., 0.]; 12]);
     
-    // // A triangle using vertices 0, 2, and 1.
-    // // Note: order matters. [0, 1, 2] will be flipped upside down, and you won't see it from behind!
-    // mesh.set_indices(Some(mesh::Indices::U32(vec![0, 2, 1, 3, 4, 5, 6, 7, 8, 9, 11, 10]))); 
-                                                //12, 14, 13, 15, 16, 17, 18, 19, 20, 21, 23, 22
-                                                //24, 26, 25, 27, 28, 29, 30, 32, 32, 33, 35, 34
+    // // // A triangle using vertices 0, 2, and 1.
+    // // // Note: order matters. [0, 1, 2] will be flipped upside down, and you won't see it from behind!
+    // // mesh.set_indices(Some(mesh::Indices::U32(vec![0, 2, 1, 3, 4, 5, 6, 7, 8, 9, 11, 10]))); 
+    //                                             //12, 14, 13, 15, 16, 17, 18, 19, 20, 21, 23, 22
+    //                                             //24, 26, 25, 27, 28, 29, 30, 32, 32, 33, 35, 34
 
-    mesh.set_indices(Some(mesh::Indices::U32(indexvec)));
+    // mesh.set_indices(Some(mesh::Indices::U32(indexvec)));
 
-    // let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
-    // treebuilder::print_hello(commands, meshes, materials);
+    let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
+    treebuilder::print_hello(commands, meshes, materials);
 
     println!("Meshes: {:?}", meshes);
 
@@ -297,31 +297,31 @@ fn setup(
 
 
 //new variant
-    // let mut meho = Treedata::new();
-    // // meho.mesh_handle();
-    // //meho.generate_mesh();
+    let mut meho = Treedata::new();
+    // meho.mesh_handle();
+    //meho.generate_mesh();
 
-    // let meshhandle = meho.mesh_handle(&mut meshes);
-
-    // let mut scalef = 1.0; 
-    // // MyMesh
-    // commands.spawn(PbrBundle {
-    //     mesh: meho.get_mesh_handle(),
-    //     material: materials.add(Color::rgb(0.6, 0.3, 0.1).into()),
-    //     transform: Transform::from_scale(Vec3{x:scalef,y:scalef,z:scalef}),
-    //     ..default()
-    // });
-
-    //oldvariant, nicht auslgelagert
+    let meshhandle = meho.mesh_handle(&mut meshes);
 
     let mut scalef = 1.0; 
-
+    // MyMesh
     commands.spawn(PbrBundle {
-        mesh: meshes.add(mesh),
+        mesh: meho.get_mesh_handle(),
         material: materials.add(Color::rgb(0.6, 0.3, 0.1).into()),
         transform: Transform::from_scale(Vec3{x:scalef,y:scalef,z:scalef}),
         ..default()
     });
+
+    //oldvariant, nicht auslgelagert
+
+    // let mut scalef = 1.0; 
+
+    // commands.spawn(PbrBundle {
+    //     mesh: meshes.add(mesh),
+    //     material: materials.add(Color::rgb(0.6, 0.3, 0.1).into()),
+    //     transform: Transform::from_scale(Vec3{x:scalef,y:scalef,z:scalef}),
+    //     ..default()
+    // });
 
 // Default Spawn of Scene Spawning ///////////////////////////////////////////////////////
 
